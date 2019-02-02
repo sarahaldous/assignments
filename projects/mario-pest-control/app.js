40ea11b4d103e8e11d7300a2a53eb60363cc021f
Sarah Aldous <sealdous@hotmail.com>
2019-02-02 10:51:32 -0700

    mario

--- projects/mario-pest-control/app.js
+++ projects/mario-pest-control/app.js
@@ -1,17 +1,15 @@
-var goombasForm = document.goombasForm
-var result = document.getElementsByTagName(div)
+var form = document.form
+var result = document.getElementById("sum")
 
-goombasForm.addEventListener("submit", function(e){
+form.addEventListener("submit", function(e){
     e.preventDefault()
-multForm.addEventListener("submit", function(e){
-    e.preventDefault()
+
     var p = document.createElement("p")
 
-    var goombaPrice = 5
 
-    var g = goombasForm.goombaQuantity.valueAsNumber
-    var b = bob-ombsForm.bobOmbsQuantity.valueAsNumber
-    var c = cheep-cheepForm.cheepCheepQuantity.valueAsNumber
+    var g = form.goombaQuantity.valueAsNumber
+    var b = form.bobOmbQuantity.valueAsNumber
+    var c = form.cheepCheepQuantity.valueAsNumber
     var sum = (5 * g) + (7 * b) + (11 * c)
     p.textContent = sum
     result.appendChild(p)
